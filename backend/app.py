21b74235089aa4ac5ef60eca93e8b6ee2ba938d2
007Unknown <hugosantos996@gmail.com>
2022-03-25 12:21:06 +0000

    added pokemon/types and pokemon/abilities endpoint to get inner join queries

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 from settings.database import DB
 import json
-
+import mysql.connector.errors as err
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
@@ -34,8 +34,87 @@ def pokemon():
     if not args:
         query = 'SELECT * FROM pokemon'
     query = " ".join([query, filters])
-    cursor = DB.cursor(dictionary=True)
+    cursor = DB.cursor()
     cursor.execute(query, tuple(data))
+    return json.dumps(cursor.fetchall())
+
+
+@app.route('/pokemon/abilities/', methods=['GET'])
+def abilities():
+    """
+    Pokémon Abilities endpoint
+    :return: returns the json information with a query depending on the args
+    """
+
+    args = request.args.to_dict()
+    print(args)
+    data = []
+    conditions = []
+
+    for arg in args:
+        conditions.append(f"{arg}=%s")
+        data.append(args.get(arg))
+
+    filters = " AND ".join(conditions)
+
+    query = """
+    SELECT rap.pokemonID, p.name, JSON_ARRAYAGG(JSON_OBJECT('name', a.ability, 'ability_type', a.abilityType))
+    FROM relAbilitiesPokemon AS rap
+    INNER JOIN pokemon AS p ON rap.pokemonID = p.ID
+    INNER JOIN abilities AS a ON rap.abilityID = a.ID
+    """
+
+    if args:
+        query = " ".join([query, "WHERE", filters, "GROUP by rap.pokemonID"])
+    else:
+        query = " ".join([query, filters, "GROUP by rap.pokemonID"])
+
+    cursor = DB.cursor()
+    try:
+        cursor.execute(query, tuple(data))
+    except err.ProgrammingError:
+        return 'Key does not exist'
+    except err.IntegrityError:
+        return 'Ambiguous key'
+    return json.dumps(cursor.fetchall())
+
+
+@app.route('/pokemon/types/', methods=['GET'])
+def types():
+    """
+    Pokémon types endpoint
+    :return: returns the json information with a query depending on the args
+    """
+
+    args = request.args.to_dict()
+    data = []
+    conditions = []
+
+    for arg in args:
+        conditions.append(f"{arg}=%s")
+        data.append(args.get(arg))
+
+    filters = " AND ".join(conditions)
+
+    query = """
+    SELECT rtp.pokemonID, p.name, JSON_ARRAYAGG(JSON_OBJECT('type', t.`type`))
+    FROM relTypesPokemon AS rtp
+    INNER JOIN pokemon AS p ON rtp.pokemonID = p.ID
+    INNER JOIN types AS t ON rtp.typeID = t.ID
+    """
+
+    if args:
+        query = " ".join([query, "WHERE", filters, "GROUP by rtp.pokemonID"])
+    else:
+        query = " ".join([query, filters, "GROUP by rtp.pokemonID"])
+
+    cursor = DB.cursor()
+    try:
+        cursor.execute(query, tuple(data))
+    except err.ProgrammingError:
+        return 'Key does not exist'
+    except err.IntegrityError:
+        return 'Ambiguous key'
     return json.dumps(cursor.fetchall())
 
 
